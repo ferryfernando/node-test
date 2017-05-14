6ac063848f0a793fa5a2cbbfe921cbb305cc7537
Ferry Fernando <ferryfernando@outlook.com>
2017-05-14 22:53:45 +0700

    Setup start scrip and heroku prot

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+var port = process.env.PORT  || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -22,9 +22,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -49,6 +49,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server run on port 3000');
+app.listen(port, () => {
+  console.log(`Server run on port ${port}`);
 });
